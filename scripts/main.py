338ee9c0872fe2c2d390f1500f92c7852159c799
Brady Blundell <bradyzemailbox@gmail.com>
2021-11-15 16:31:41 -0800

    Update main.py
    Updated the previous wxPython code to add a vertical splitter in the window. It is also on the correct branch this time.

--- scripts/main.py
+++ scripts/main.py
@@ -1,30 +1,101 @@
-
-#!/usr/bin/env python
-
 import wx
-import htt_viz_py.tree as t
 
-class MainWindow(wx.Frame):
-    def __init__(self, parent, title):
-        wx.Frame.__init__(self, parent, title=title, size=(200,100))
-        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
-        self.CreateStatusBar() # A Statusbar in the bottom of the window
+class frameMain ( wx.Frame ):
+	
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"HTT-VIZ", pos = wx.DefaultPosition, size = wx.Size( 1000,550 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+		
+		## Split window into panels
+		splitter = wx.SplitterWindow(self)
+		left = NodePanel(splitter)
+		right = TreePanel(splitter)
+		splitter.SplitVertically(left, right)
+		splitter.SetMinimumPaneSize(200)
+		
+		self.Layout()
+		self.menubarMain = wx.MenuBar( 0 )
+		self.menuFile = wx.Menu()
+		self.menuItemFileNew = wx.MenuItem( self.menuFile, wx.ID_ANY, u"New"+ u"\t" + u"Ctrl+N", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuFile.AppendItem( self.menuItemFileNew )
+		
+		self.menuItemFileOpen = wx.MenuItem( self.menuFile, wx.ID_ANY, u"Open"+ u"\t" + u"Ctrl+O", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuFile.AppendItem( self.menuItemFileOpen )
+		
+		self.menuItemFileSave = wx.MenuItem( self.menuFile, wx.ID_ANY, u"Save"+ u"\t" + u"Ctrl+S", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuFile.AppendItem( self.menuItemFileSave )
+		
+		self.menuItemFileSaveAs = wx.MenuItem( self.menuFile, wx.ID_ANY, u"Save As"+ u"\t" + u"Ctrl-Shift+S", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuFile.AppendItem( self.menuItemFileSaveAs )
+		
+		self.menuFile.AppendSeparator()
+		
+		self.menuItemFileExit = wx.MenuItem( self.menuFile, wx.ID_ANY, u"Exit"+ u"\t" + u"Alt-F4", wx.EmptyString, wx.ITEM_NORMAL )
+		self.menuFile.AppendItem( self.menuItemFileExit )
+		
+		self.menubarMain.Append( self.menuFile, u"File" ) 
+		
+		self.menuEdit = wx.Menu()
+		self.menubarMain.Append( self.menuEdit, u"Edit" ) 
+		
+		self.menuView = wx.Menu()
+		self.menubarMain.Append( self.menuView, u"View" ) 
+		
+		self.m_menuHelp = wx.Menu()
+		self.menubarMain.Append( self.m_menuHelp, u"Help" ) 
+		
+		self.SetMenuBar( self.menubarMain )
+		
+		
+		self.Centre( wx.BOTH )
+		
+		# Connect Events
+		self.Bind( wx.EVT_MENU, self.menuItemFileNewOnMenuSelection, id = self.menuItemFileNew.GetId() )
+		self.Bind( wx.EVT_MENU, self.menuItemFileOpenOnMenuSelection, id = self.menuItemFileOpen.GetId() )
+		self.Bind( wx.EVT_MENU, self.menuItemFileSaveOnMenuSelection, id = self.menuItemFileSave.GetId() )
+		self.Bind( wx.EVT_MENU, self.menuItemFileSaveAsOnMenuSelection, id = self.menuItemFileSaveAs.GetId() )
+		self.Bind( wx.EVT_MENU, self.menuItemFileExitOnMenuSelection, id = self.menuItemFileExit.GetId() )
+	
+	def __del__( self ):
+		pass
+	
+	
+	# Virtual event handlers, overide them in your derived class
+	def menuItemFileNewOnMenuSelection( self, event ):
+		event.Skip()
+	
+	def menuItemFileOpenOnMenuSelection( self, event ):
+		event.Skip()
+	
+	def menuItemFileSaveOnMenuSelection( self, event ):
+		event.Skip()
+	
+	def menuItemFileSaveAsOnMenuSelection( self, event ):
+		event.Skip()
+	
+	def menuItemFileExitOnMenuSelection( self, event ):
+		event.Skip()
 
-        # Setting up the menu.
-        filemenu= wx.Menu()
+class NodePanel(wx.Panel):
+	def __init__(self, parent):
+		wx.Panel.__init__(self, parent = parent)
+		wx.Button(self, -1, "New Node")
+		self.SetBackgroundColour("grey")
 
-        # wx.ID_ABOUT and wx.ID_EXIT are standard IDs provided by wxWidgets.
-        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
-        filemenu.AppendSeparator()
-        filemenu.Append(wx.ID_EXIT,"E&xit"," Terminate the program")
+class TreePanel(wx.Panel):
+	def __init__(self, parent):
+		wx.Panel.__init__(self, parent = parent)
+		wx.Button(self, -1, "Run Tree")
+		self.SetBackgroundColour("dark grey")
+	
+class MainApp(wx.App):
+    def OnInit(self):
+        mainFrame = frameMain(None)
+        mainFrame.Show(True)
+        return True
 
-        # Creating the menubar.
-        menuBar = wx.MenuBar()
-        menuBar.Append(filemenu,"&File") # Adding the "filemenu" to the MenuBar
-        self.SetMenuBar(menuBar)  # Adding the MenuBar to the Frame content.
-        self.Show(True)
+if __name__ == '__main__':
+    app = MainApp()
+    app.MainLoop()
 
-a = t.Tree()
-app = wx.App(False)
-frame = MainWindow(None, "Sample editor")
-app.MainLoop()
