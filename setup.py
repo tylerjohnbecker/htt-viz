2ea3552e474b0d07cb44befb3342e9a7132c73dd
Nathaniel Daniel <nathanieldaniel@nevada.unr.edu>
2022-02-18 19:23:10 -0800

    Document setup.py slightly

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
-# nothing right now but like don't run this
+#!/usr/bin/env python3
+
+# Setup file for building this package.
+# You shouldn't run this manually.
+# See `http://docs.ros.org/en/jade/api/catkin/html/user_guide/setup_dot_py.html` for more info.
 
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
@@ -7,7 +11,7 @@ d = generate_distutils_setup(
     packages=[
 		'htt_viz_py', 
 		
-		# Hope this wan't important, it broke my build
+		# Nathaniel: Hope this wan't important, it broke my build
 		# 'rosgraph_msgs', 
 		# 'qt_gui', 
 		# 'python_qt_binding', 
