5a59c0b2e5bf64ef98acd006979e4019003cf34e
Nathaniel Daniel <nathanieldaniel@nevada.unr.edu>
2022-02-18 23:46:15 -0800

    Add MockUp .ui File. THIS IS NOT FUNCTIONAL, DO NOT USE IT!

--- src/htt_viz_rqt/my_module.py
+++ src/htt_viz_rqt/my_module.py
@@ -6,6 +6,8 @@ from qt_gui.plugin import Plugin
 from python_qt_binding import loadUi
 from python_qt_binding.QtWidgets import QWidget
 
+ROS_PACKAGE_NAME = 'htt_viz'
+
 class MyPlugin(Plugin):
     def __init__(self, context):
         super(MyPlugin, self).__init__(context)
@@ -27,7 +29,7 @@ class MyPlugin(Plugin):
         # Create QWidget
         self._widget = QWidget()
         # Get path to UI file which should be in the "resource" folder of this package
-        ui_file = os.path.join(rospkg.RosPack().get_path('htt_viz'), 'resource', 'MyPlugin.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path(ROS_PACKAGE_NAME), 'resource', 'MyPlugin.ui')
         # Extend the widget with all attributes and children from UI file
         loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
