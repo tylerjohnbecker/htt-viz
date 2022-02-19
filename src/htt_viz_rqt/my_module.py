0fd4d428cc735bdef260a8338c2c2e96ff963913
Nathaniel Daniel <nathanieldaniel@nevada.unr.edu>
2022-02-18 20:06:25 -0800

    Load a .ui file from the resources folder

--- src/htt_viz_rqt/my_module.py
+++ src/htt_viz_rqt/my_module.py
@@ -27,9 +27,9 @@ class MyPlugin(Plugin):
         # Create QWidget
         self._widget = QWidget()
         # Get path to UI file which should be in the "resource" folder of this package
-        #ui_file = os.path.join(rospkg.RosPack().get_path('rqt_mypkg'), 'resource', 'MyPlugin.ui')
+        ui_file = os.path.join(rospkg.RosPack().get_path('htt_viz'), 'resource', 'MyPlugin.ui')
         # Extend the widget with all attributes and children from UI file
-        #loadUi(ui_file, self._widget)
+        loadUi(ui_file, self._widget)
         # Give QObjects reasonable names
         self._widget.setObjectName('MyPluginUi')
         # Show _widget.windowTitle on left-top of each plugin (when 
