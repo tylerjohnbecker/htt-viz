04df1219cbf8b8f618c90c15b666b1a6cfc269a3
tylerjohnbecker <tylerjohnbecker@gmail.com>
2022-02-23 10:31:07 -0800

    Added the basics for the backend of Params. The four types of Params are possible, and so far the only feature implemented is a written and tested algo. for reading and writing param files. However, the Param class should be fine just as it is with only some members, and an equality function. Everything else will be handled by the NodeType class.

--- src/htt_viz_py/Param.py
+++ src/htt_viz_py/Param.py
@@ -1,44 +1,52 @@
 #!/usr/bin/python
 
 import yaml
-from yaml import Loader
+from yaml import Loader, Dumper
 
-class Param:
+class Param(object):
 
     def __init__(self):
         self.name = ""
         self.type = "none"
+        self.value = None
+
+    def equals(self, comp_param):
+        return self.name == comp_param.name \
+            and self.type == comp_param.type
 
 class ParamFloat (Param):
 
     def __init__(self):
-        super().__init__()
+        super(Param, self).__init__()
 
         self.type = "float"
+        self.value = 0.0
 
-    #etcs
 
 class ParamString (Param):
     
     def __init__(self):
-        super().__init__()
+        super(Param, self).__init__()
 
         self.type = "string"
+        self.value = ""
 
 
 class ParamInt (Param):
     
     def __init__(self):
-        super().__init__()
+        super(Param, self).__init__()
 
         self.type = "int"
+        self.value = 0
 
 class ParamBool (Param):
     
     def __init__(self):
-        super().__init__()
+        super(Param, self).__init__()
 
         self.type = "bool"
+        self.value = False
 
 
 def read            (file_name):
@@ -46,8 +54,6 @@ def read            (file_name):
     
     with open(file_name, "r") as file: 
         data = yaml.load(file, Loader=Loader)
-        print(data["node"]["source"] + ": ")
-        print("Expected values from file: ")
         for par_name in data["node"]["params"]["param_names"]:
             par_type = data["node"]["params"][par_name]
 
@@ -96,15 +102,53 @@ def test_read       (file_name):
         for x in param_list:
             print("[" + x.name + "]: [" + x.type + "]")
 
-def write           (file_name):
-    pass
+#wrapper object for this will need to hold the src file as a member of the class
+def write           (params, file_name):
+    
+    yamlDict = {}
+
+    yamlDict["node"] = {}
+
+    yamlDict["node"]["source"] = "move_behavior.h" 
+
+    yamlDict["node"]["params"] = {}
+    yamlDict["node"]["params"]["param_names"] = []
+
+    for x in params:
+        yamlDict["node"]["params"]["param_names"].append(x.name)
+        yamlDict["node"]["params"][x.name] = x.type
+
+    with open(file_name, "w") as file:
+        yaml.dump(yamlDict, file)
+
 
 def test_write      (file_name):
-    pass
+    params = read(file_name)
+
+    write(params, "/home/tylerbecker/sp_ws/src/htt-viz/include/behavior/test.yaml")
+
+    params2 = read("/home/tylerbecker/sp_ws/src/htt-viz/include/behavior/test.yaml")
+
+    passed = True
+
+    if not len(params) == len(params2):
+        passed = False
+        print("Test failed, after a write and read the number of params are not the same!")
+
+    for i in range(len(params)):
+        passed = passed and (params[i].equals(params2[i]))
+
+        if not passed:
+            print("Test failed, after a write and a read the params are no longer equivalent!")
+
+            break
+
+    if passed:
+        print("Test succeeded after a read-write-read the same data was loaded into the list!")
 
 if __name__ == "__main__":
     #test the class
 
     # more accurately test the init function of the class's wrapper, by essentially 
     # creating a list here from a file
-    test_read("C:/opt/workspace/catkin_ws/src/htt_viz/include/behavior/move_behavior.yaml")
+    test_write("/home/tylerbecker/sp_ws/src/htt-viz/include/behavior/move_behavior.yaml")
