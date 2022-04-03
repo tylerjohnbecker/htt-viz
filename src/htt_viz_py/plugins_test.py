bcb6c9bd05e4ca2ac57a01b79e9c5dc81bfc61a5
Alex Tello <73854603+goldenchurro@users.noreply.github.com>
2022-04-02 17:50:32 -0700

    Update plugins_test.py

--- src/htt_viz_py/plugins_test.py
+++ src/htt_viz_py/plugins_test.py
@@ -1,8 +1,7 @@
 import pytest
-from pytestqt.exceptions import 
-(
+from pytestqt.exceptions import (
   _is_exception_capture_enable,
-  _QtExceptionCaptureManager
+  _QtExceptionCaptureManager,
 )
 
 from pytestqt.logging import QtLoggingPlugin, QtMessageCapture
@@ -34,3 +33,49 @@ def qapp(qapp_args, pytestconfig):
     
     result = QtBot(request)
     return result
+  
+  def qtmodeltester(request)
+  
+  from pytestqt.modeltest import ModelTester
+   tester = ModelTester(request.config)
+    yield tester 
+    tester._cleanup()
+   
+def pytest_addoption(parser):
+    parser.addini (
+  
+      
+def pytest_rutest_setup(item)
+      capture_enabled = _is_exception_capture_enable(item)
+      
+def pytest_runtest_call(item)
+      yield 
+      _process_events()
+      capture_enabled = _is_exception_capture_enabled(item)
+      
+def pytest_runtest_teardown(item)
+      
+      """ 
+      This allows any pending events for the test tear down as well as
+      avoiding any events leaking into the next set of tests. If there are 
+      any exceptions that have been captured during this test, we fail the test 
+      """
+      
+      _process_events()
+      _close_widgets(item)
+      _process_events()
+      yield
+      _process_events()
+      capture_enabled = _is_exception_capture_enabled(item)
+      if capture_enabled:
+      
+def _process_events():
+      app = qt_api.QWidgets.QApplication.instance()
+  if app is not None:
+    app.processEvents()
+      
+def pytest_configure(config):
+      config.addinivalue_line()
+      
+def pytest_report_header():
+  from pytest.qt
