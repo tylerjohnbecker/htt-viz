67b81f4c74c5079021b9e6914608868d0311a032
Alex Tello <73854603+goldenchurro@users.noreply.github.com>
2022-04-13 22:11:09 -0700

    Create test_wait.py

---
+++ src/htt_viz_py/test_wait.py
@@ -0,0 +1,63 @@
+import pytest
+
+from pytestqt.exception import TimeoutError
+
+def test_wait(qtbot, wait_4_ticks_callback, tick_counter):
+   tick_counter.start(100)
+   qtbot.waitUntil(wait_4_ticks_callback, timeout = 1000)
+   assert tick_counter.ticks >= 4
+    
+    
+def test_wait_until timeout(qtbot, wait_4_ticks_callback, tick_counter):
+      tick_counter.start(200)
+      with pytest.raises(TimeoutError):
+        qtbot.waitUntil(wait_4_ticks_callback, timeout = 100)
+      assert tick_counter.ticks < 4
+      
+      
+      
+def test_invaild_callback_return_value(qtbot):
+  with pytest.raises(ValueError):
+    qtbot.waitUntil(lambda: [])
+    
+    
+@pytest.fixture(params = ["predicate", "assert"])
+def wait_4_ticks_callback(request, tick_counter):
+  if request param == "predicate":
+    return lambda: tick_counter.ticks >= 4
+  else: 
+    
+    return check_ticks
+  
+@pytest.fixture
+def tick_counter():
+  
+  from pytest.qt_combat import qt_api
+  
+  class Counter:
+    def __init__(self):
+      self._ticks = 0
+      self.timer = qt_api.QtCore.QTimer()
+      self.timer.timeout.connect(self._tick)
+      
+    def start(self, ms):
+        self.timer.start(ms)
+        
+    def _tick(self):
+      self.ticks += 1
+      
+    @property
+    def ticks(self):
+      return self._ticks
+    
+  counter = Counter()
+  yield counter()
+  yield counter
+  counter.timer.stop()
+
+
+
+
+
+
+
