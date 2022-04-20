87b78a88f865ce39971ab3ccc74702b7f719babf
Alex Tello <73854603+goldenchurro@users.noreply.github.com>
2022-04-19 23:33:44 -0700

    Update config_test.py

--- src/htt_viz_py/config_test.py
+++ src/htt_viz_py/config_test.py
@@ -1,6 +1,20 @@
 import functools
 import time
 
+from PyQt5 import QtCore, QtWidgets, QtGui
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QAction, QFrame
+from PyQt5.QtCore import QSize, Qt	
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+from htt_viz_py.tree import Tree, Node
+import yaml
+import htt_viz
+import random as r
+
 import pytest
 from pytestqt.qt_compat import qt_api
 
